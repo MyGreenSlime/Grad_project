c6b414b902ff5f4eb2e55ca9e3149e7461d1c8a7
don <ruksachai@gmail.com>
2020-03-01 00:27:49 +0700

    first_commit

---
+++ frontend/src/App.vue
@@ -0,0 +1,59 @@
+<template>
+  <div id="app">
+    <div class="dashboard">
+      <VisualizeSurface :Title ="title" :ZTitle="ztitle" :DataSet="dataset"></VisualizeSurface>
+    </div>
+    <div class="buttons_set">
+      <button @click="changeDataset1">Density Average</button>
+      <button @click="changeDataset2">Melt Flow Index Average</button>
+      <button @click="changeDataset3">Melt Flow Index Max</button>
+      <button @click="changeDataset4">Density Max</button>
+    </div>
+    
+    
+  </div>
+</template>
+
+<script>
+
+import VisualizeSurface from './components/visualize_surface'
+export default {
+  name: 'App',
+  components: {
+    VisualizeSurface,
+  },
+  data : function(){
+    return {
+      dataset : "",
+      title : "",
+      ztitle : ""
+    }
+  },
+  methods: {
+    changeDataset1 : function(){
+      this.dataset = "avg_density",
+      this.title = "Density Average",
+      this.ztitle = "Density"
+    },
+    changeDataset2 : function(){
+      this.dataset = "avg_mi",
+      this.title = "Melt Flow Index Average",
+      this.ztitle = "Melt Flow Index"
+    },
+    changeDataset3 : function(){
+      this.dataset = "max_mi",
+      this.title = "Melt Flow Index Max",
+      this.ztitle = "Melt Flow Index"
+    },
+    changeDataset4 : function(){
+      this.dataset = "max_density",
+      this.title = "Density Max",
+      this.ztitle = "Density"
+    }
+  },
+}
+</script>
+
+<style>
+
+</style>
---
+++ frontend/vue.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    chainWebpack: config => {
+      config
+        .module
+        .rule("csv")
+        .test(/\.csv$/)
+        .use("file-loader")
+        .loader("file-loader")
+        .options({
+            name: "[path][name].[ext]"
+        })
+        .end();
+    }
+  }
